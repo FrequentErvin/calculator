4bdc7e90c821f2741170b904a204a307ac60c523
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-09 15:19:41 +0200

    Initial structure of calculator, no logic yet

---
+++ script.js
@@ -0,0 +1,30 @@
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a - b;
+}
+function multiply(a,b){
+    return a * b;
+}
+function divide(a,b){
+    if(b == 0) return "UNDEFINED";
+    return a / b;
+}
+function operate(operator,a,b){
+    let result = 0;
+    switch(operator){
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;
+    }
+}
