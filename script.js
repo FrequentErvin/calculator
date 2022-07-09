4e34a9d28e9a43a019d8920e4b09cd1607e3efdd
FrequentErvin <ervin.macic@2gimnazija.edu.ba>
2022-07-09 17:54:01 +0200

    Works but scuffed, fixing currently

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let currentNumber = 0;
 let firstNumber = "";
 let secondNumber = "";
 let currentOperation = "";
-
+let wipeDisplayDown = false;
+let Result = 0;
+let justEvald = false;
 const displayUp = document.querySelector(".display-up");
 const displayDown = document.querySelector(".display-down");
 const numberButtons = document.querySelectorAll(".number");
@@ -26,18 +28,35 @@ operationButtons.forEach((operationButton) =>
     operationButton.addEventListener("click", () => setOperation(operationButton.textContent))
 );
 function addToDisplay(number){
-    displayUp.textContent += number;
-    displayDown.textContent = number;
+    if(justEvald == true){
+        displayUp.textContent = Result;
+        displayDown.textContent = "";
+        justEvald = false;
+    }
+    if(currentOperation == ""){
+        displayUp.textContent = "";
+    }
+    if(wipeDisplayDown) wipeDisplayDown = false, displayDown.textContent = "";
+    if(displayDown.textContent == "0"){
+        displayDown.textContent = number;
+    }else{
+        displayDown.textContent += number;
+    }
 }
 function setOperation(operator){
+    displayUp.textContent += displayDown.textContent;
    if(currentOperation != "") evaluate();
-   currentOperation = operator;
    if(operator != "x^y"){
+    if(justEvald) displayUp.textContent = Result + "^";
     displayUp.textContent += operator;
+    currentOperation = operator;
    }else{
+    if(justEvald) displayUp.textContent = Result + currentOperation;
     displayUp.textContent += "^";
+    currentOperation = "^";
    }
-
+   firstNumber = displayDown.textContent;
+   wipeDisplayDown = true;
 }
 function AC(){
     displayUp.textContent = "";
@@ -45,7 +64,11 @@ function AC(){
     currentOperation = "";
 }
 function add(a,b){
-    return a + b;
+    console.log(a);
+    console.log(b);
+    let c = a + b;
+    console.log(a+b);
+    return c;
 }
 function subtract(a,b){
     return a - b;
@@ -62,7 +85,12 @@ function power(a,b){
     return a ** b;
 }
 function evaluate(){
-    secondNumber = displayUp.textContent;
+    secondNumber = displayDown.textContent;
+    displayDown.textContent = operate(currentOperation, firstNumber, secondNumber);
+    displayUp.textContent = `${firstNumber} ${currentOperation} ${secondNumber} = `;
+    currentOperation = ""; 
+    Result = displayDown.textContent;
+    justEvald = true;
 }
 function addPoint(){
 
@@ -71,22 +99,27 @@ function deleteChar(){
 
 }
 function operate(operator,a,b){
+    a = Number(a);
+    b = Number(b);
     let result = 0;
+    console.log(result);
     switch(operator){
         case "+":
-            result = add(a,b);
+            result = +add(a,b);
             break;
         case "-":
-            result = subtract(a,b);
+            result = +subtract(a,b);
             break;
         case "*":
-            result = multiply(a,b);
+            result = +multiply(a,b);
             break;
         case "/":
-            result = divide(a,b);
+            result = +divide(a,b);
             break;
         case "^":
-            result = power(a,b);
+            result = +power(a,b);
             break;
     }
+    console.log(result);
+    return result;
 }
